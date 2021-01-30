2e35dd57cece5628c28510f54edf97f78b92909e
Anthony <35087098+agrusk@users.noreply.github.com>
2021-01-29 22:03:08 -0500

    Concept level added

---
+++ GlobalGameJam/Assets/Scripts/EntrenceText.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EntrenceText : MonoBehaviour
+{
+
+    public GameObject text;
+    void OnTriggerEnter(Collider other){
+        text.SetActive(true);
+    }
+
+    void OnTriggerExit(Collider other){
+        text.SetActive(false);
+    }
+}
