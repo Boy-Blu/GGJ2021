4a337e4ccb5757a44e30ef6ae3a2c26cb596081e
Anthony <35087098+agrusk@users.noreply.github.com>
2021-01-30 22:25:54 -0500

    added wwise walk step sounds

--- GlobalGameJam/Assets/Scripts/PlayerController.cs
+++ GlobalGameJam/Assets/Scripts/PlayerController.cs
@@ -81,6 +81,12 @@ public class PlayerController : MonoBehaviour
     private float MinFov;
     public float FOVSpeed; //how fast we must go before we reach max fov
 
+    [Header("Audio")]
+    public float footStepTime = 0.33f;
+    private float footStepTimer = 0f;
+
+
+    [Header("Other")]
     //Word information
     public string currentWord;
 
@@ -187,7 +193,7 @@ public class PlayerController : MonoBehaviour
             //clamp our rigid velocity to nothing
             Rigid.velocity = Vector3.zero;
         }
-
+        playFootStepAudio();
     }
 
 
@@ -284,6 +290,7 @@ public class PlayerController : MonoBehaviour
 
             //turn our player with the in air modifier
             TurnPlayer(CamX, Del, TurnSpeedInAir);
+            footStepTimer = 0f;
         }
         else if (CurrentState == PlayerStates.OnWalls)
         {
@@ -548,5 +555,17 @@ public class PlayerController : MonoBehaviour
 
         //slide in direction
         Rigid.AddForce(transform.forward * SlideAmt, ForceMode.Impulse);
+    }
+
+    private void playFootStepAudio(){
+        if(CurrentState == PlayerStates.Grounded){
+            // Run case (As Sample)
+            if (footStepTimer < 0 && (Math.Abs(Rigid.velocity.x) > 0.1f || Math.Abs(Rigid.velocity.z) > 0.1f)){
+                AkSoundEngine.PostEvent("footstepsEvent", gameObject);
+                footStepTimer = footStepTime;
+            }else{
+                footStepTimer -= Time.deltaTime;
+            }
+        }
     }
 }
