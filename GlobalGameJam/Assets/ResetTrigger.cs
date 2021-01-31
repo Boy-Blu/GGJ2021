932925f461f11e2aacb4f931fbd96905f70a1b5e
Omri Cundangan <37829365+dlrht@users.noreply.github.com>
2021-01-31 14:28:00 -0500

    Added Trigger Reset prefab & colour mats

---
+++ GlobalGameJam/Assets/ResetTrigger.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetTrigger : MonoBehaviour
+{
+    public Transform SpawnPos;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        other.transform.position = SpawnPos.transform.position;
+    }
+}
